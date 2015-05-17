f3b7f143a826cc81a8230158b731fa441a25d7f6
X4dr <rosch@zedat.fu-berlin.de>
2015-05-17 03:37:19 +0200

    started work on multiuser

--- NossiNet.py
+++ NossiNet.py
@@ -50,6 +50,21 @@ def add_entry():
     flash('New entry was successfully posted')
     return redirect(url_for('show_entries'))
 
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('register.html', error=error)
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
