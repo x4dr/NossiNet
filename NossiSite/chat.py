ce7085178eb119f5b792c8c3e2ebca99707b6c16
x4dr <x4dr@hotmail.com>
2016-01-02 12:07:29 +0100

    starting to get chat into shape^

--- NossiSite/chat.py
+++ NossiSite/chat.py
@@ -1,103 +1,97 @@
 from NossiSite import app, socketio
 import time
 import threading
-Thread = threading.Thread
-from flask import Flask, render_template, session, request
-from flask_socketio import SocketIO, emit, join_room, leave_room, \
+from flask import render_template, session, request
+from flask_socketio import  emit, join_room, leave_room, \
     close_room, rooms, disconnect
-
-#app = Flask(__name__)
-#app.config['SECRET_KEY'] = 'ajdjJFeiJjFnnm88e4ko94VBPhzgY34'
-#app.config.from_object(__name__)
-#socketio = SocketIO(app, async_mode=async_mode)
 thread = None
 
 
 def background_thread():
     """Example of how to send server generated events to clients."""
     count = 0
-    while True:
-        time.sleep(10)
+    while count <2:
+        print ("sending status")
+        time.sleep(1)
         count += 1
-        socketio.emit('my response',
-                      {'data': 'Server generated event', 'count': count},
-                      namespace='')
+        socketio.emit('Update',
+                      {'data': 'nm', 'count': count},
+                      namespace='/update')
+    global thread
+    thread = None
 
 
 @app.route('/chat/')
 def chatsite():
     global thread
     if thread is None:
-        thread = Thread(target=background_thread)
+        thread = threading.Thread(target=background_thread)
         thread.daemon = True
         thread.start()
     return render_template('chat.html')
 
 
-@socketio.on('my event', namespace='')
+@socketio.on('ClientServerEvent', namespace='/chat')
 def test_message(message):
+    print("ClientServerEvent ",message)
     session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my response',
+    print(message['data'])
+    emit('Message',
          {'data': message['data'], 'count': session['receive_count']})
 
 
-@socketio.on('my broadcast event', namespace='')
+@socketio.on('BroadCastEvent', namespace='/chat')
 def test_broadcast_message(message):
+    print("Broadcast",message)
     session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my response',
+    emit('Message',
          {'data': message['data'], 'count': session['receive_count']},
          broadcast=True)
 
 
-@socketio.on('join', namespace='')
+@socketio.on('Join', namespace='/chat')
 def join(message):
+    print("Join",message)
     join_room(message['room'])
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my response',
+    sessin['receive_count'] = session.get('receive_count', 0) + 1
+    emit('Message',
          {'data': 'In rooms: ' + ', '.join(rooms()),
           'count': session['receive_count']})
 
 
-@socketio.on('leave', namespace='')
+@socketio.on('Leave', namespace='/chat')
 def leave(message):
+    print("Leave",message)
     leave_room(message['room'])
     session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my response',
+    emit('Message',
          {'data': 'In rooms: ' + ', '.join(rooms()),
           'count': session['receive_count']})
 
 
-@socketio.on('close room', namespace='')
-def close(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my response', {'data': 'Room ' + message['room'] + ' is closing.',
-                         'count': session['receive_count']},
-         room=message['room'])
-    close_room(message['room'])
-
-
-@socketio.on('my room event', namespace='')
+@socketio.on('RoomEvent', namespace='/chat')
 def send_room_message(message):
+    print("RoomEvent",message)
     session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my response',
+    emit('Message',
          {'data': message['data'], 'count': session['receive_count']},
          room=message['room'])
 
 
-@socketio.on('disconnect request', namespace='')
+@socketio.on('Disconnect', namespace='/chat')
 def disconnect_request():
     session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my response',
+    emit('Message',
          {'data': 'Disconnected!', 'count': session['receive_count']})
     disconnect()
 
 
-@socketio.on('connect', namespace='')
+@socketio.on('connect', namespace='/chat')
 def test_connect():
-    emit('my response', {'data': 'Connected', 'count': 0})
+    emit('Message', {'data': 'Connected', 'count': 0})
 
 
-@socketio.on('disconnect', namespace='')
+@socketio.on('disconnect', namespace='/chat')
 def test_disconnect():
     print('Client disconnected', request.sid)
 
