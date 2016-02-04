e54869bf77daae9c40aa892235f0082c06c67d2d
x4dr <x4dr@hotmail.com>
2016-02-04 13:32:23 +0100

    toying with autoupdating server

--- NossiNet.py
+++ NossiNet.py
@@ -1,5 +1,8 @@
-# from flask_socketio import SocketIO
+from NossiPack.RollbackImporter import RollbackImporter
+
+RbI = RollbackImporter()
 
 from NossiSite import app, socketio
+
 
 socketio.run(app, "0.0.0.0", debug=False, port=80)
