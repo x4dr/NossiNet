548c41a2c55f65e5eb8725b6b0d979913c654446
x4dr <x4dr@hotmail.com>
2016-03-12 11:48:00 +0100

    mapstuff

--- NossiSite/views.py
+++ NossiSite/views.py
@@ -112,16 +112,6 @@ def testest():
 
 @app.route('/map')
 def berlinmap():
-    print ("encoding picture")
-    with open(os.path.dirname(os.path.realpath(__file__)) + "/static/berlin.jpg", 'rb') as image_file:
-        encoded_string = base64.b64encode(image_file.read())
-        encoded_string = str(encoded_string)[2:-1]
-    print ("sending picture")
-    return Response(stream_string('<img src="data:image/jpg;base64,{0}'.format(encoded_string) + '">'))
-
-
-@app.route('/maplarge')
-def berlinlargemap():
     with open(os.path.dirname(os.path.realpath(__file__)) + "/static/berlin.png", 'rb') as image_file:
         encoded_string = base64.b64encode(image_file.read())
         encoded_string = str(encoded_string)[2:-1]
