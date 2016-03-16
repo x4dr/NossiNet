7a72c2439a98ce8fd2ccccf1c69b35db9140a3b7
x4dr <x4dr@hotmail.com>
2016-03-16 13:45:33 +0100

    gunicorn integration

--- NossiNet.py
+++ NossiNet.py
@@ -1,10 +1,13 @@
-# from NossiPack.RollbackImporter import RollbackImporter
+
 import sys
 from NossiSite import app, socketio
+
+
 # print(os.getcwd()) this is where the database will be, allowing for multiple servers to run on different databases
 try:
     port = int(sys.argv[1])
 except:
     port = 5000
 
-socketio.run(app, "0.0.0.0", debug=(port!=80), port=port)
+if __name__ == "__main__":
+    socketio.run(app, "0.0.0.0", debug=(port!=80), port=port)
