e4a0dd820be437d5be0141958e134615f1a9d2d3
x4dr <x4dr@hotmail.com>
2016-03-27 06:41:59 +0200

    fixed problem with keyfile location

--- NossiSite/__init__.py
+++ NossiSite/__init__.py
@@ -4,9 +4,7 @@ import os
 import random
 import string
 
-
 from flask_socketio import SocketIO
-
 
 async_mode = 'eventlet'
 eventlet.monkey_patch()
@@ -14,17 +12,22 @@ eventlet.monkey_patch()
 app = Flask(__name__)
 
 DATABASE = './NN.db'
-try:
-    with open(os.path.join(os.path.expanduser('~'),'key'), 'r') as keyfile:
-        key = keyfile.read()
-except:
-    with open(os.path.join(os.path.expanduser('~'), 'key'), 'w') as keyfile:
-        keyfile.write(''.join(random.SystemRandom().choice(string.printable) for _ in range(48)))
+i = 10
+while i < 10:
+    key=""
+    try:
+        with open(os.path.join(os.path.expanduser('~'), 'key'), 'r') as keyfile:
+            key = keyfile.read()
+    except:
+        with open(os.path.join(os.path.expanduser('~'), 'key'), 'w') as keyfile:
+            keyfile.write(''.join(random.SystemRandom().choice(string.printable) for _ in range(48)))
+    i+=1
+    if key != "":
+        i += 9
 
-SECRET_KEY = key  #should invalidate cookies
+SECRET_KEY = key  # should invalidate cookies
 app.config.from_object(__name__)
 socketio = SocketIO(app, async_mode=async_mode)
-
 
 import NossiSite.views
 import NossiSite.chat
