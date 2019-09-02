9d29ec60cb3368e6d6799a8a5b2ac57c77647dda
x4dr <x4dr@hotmail.com>
2019-09-02 09:38:22 +0200

    typo

--- fengraph.py
+++ fengraph.py
@@ -73,7 +73,7 @@ def supply_graphdata():
                 damages = ast.literal_eval(f.read())
             else:
                 print(
-                    f"fengraph comparing hashes: {str(nmd5).strip()} != {str(wmd5).strip()}, so graphdata will be "
+                    f"fengraph comparing hashes: {str(nmd5).strip()} != {str(wmd5).strip()}, so graphdata will be " +
                     f"regenerated")
                 damages = {}
     except SyntaxError as e:
