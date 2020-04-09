a0451e7ad0e4129c8df3f013fa8f493b75eb2ef1
x4dr <x4dr@hotmail.com>
2020-04-09 21:31:07 +0200

    sheet set unused

--- NossiPack/FenCharacter.py
+++ NossiPack/FenCharacter.py
@@ -137,6 +137,7 @@ class FenCharacter:
                 sel = self.parse_part(
                     "\n".join(["\n".join(x) for x in self.Meta[k].values()]), True
                 ).get(name, None)
+                print(k)
                 if sel:
                     res += self.parse_xp(sel)
         return res
@@ -145,6 +146,7 @@ class FenCharacter:
     def parse_xp(s):
         res = 0
         paren = ""
+        print(s)
         while paren != s:
             if paren:
                 pos = s.find(paren)
@@ -156,7 +158,7 @@ class FenCharacter:
                 res += 1 + paren.count(",")
             s = s.replace("[" + paren + "]", "", 1)
             paren = fullparenthesis(s, "[", "]")
-
+        print(s)
         res = sum([1 for x in s if x.strip()])
         return res
 
