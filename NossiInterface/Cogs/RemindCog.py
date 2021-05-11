7f85fc2bd579b9b702afce636bf3627d0838a3c0
Maric <x4dr@hotmail.com>
2021-05-12 01:03:13 +0200

    fix reminders, now with sql (some refactoring still needed)

--- NossiInterface/Cogs/RemindCog.py
+++ NossiInterface/Cogs/RemindCog.py
@@ -1,22 +1,59 @@
-from discord.ext import commands
+import asyncio
+import datetime
+import time
+from typing import re
+
+from discord import TextChannel
+from discord.ext import commands, tasks
+from discord.ext.commands import Bot
+from pytz import reference
 
 from NossiInterface.Tools import mentionreplacer
-from NossiInterface.reminder import delreminder, newreminder, listreminder
+from NossiInterface.reminder import (
+    delreminder,
+    newreminder,
+    listreminder,
+    next_reminders,
+)
 
 
 class RemindCog(commands.Cog, name="Remind"):
     def __init__(self, client):
-        self.client = client
+        self.client: Bot = client
+        self.reminding.start()
+        self.last_remind = 0
 
-    @commands.Cog.listener()
-    async def onready(self):
-        info = await self.client.application_info()
-        await info.owner.send("Wörkwörk")
+    @tasks.loop(minutes=1)
+    async def reminding(self):
+        repeat = True
+        if time.time() - self.last_remind < 60:
+            print("double reminder :O")
+            return
+
+        while repeat:
+            repeat = False  # probably wont pull reminders more than once
+            nr = next_reminders()
+            for r in nr:  # pull the next relevant reminders
+                delta = r[2] - time.time()
+                if delta > 60:
+                    break  # nothing within a minute, we will be called again before its time
+                else:
+                    await asyncio.sleep(
+                        delta
+                    )  # since reminders are in order we consume them in order
+                    channel: TextChannel = self.client.get_channel(r[1])
+                    if not channel:
+                        break  # not connected, try later
+                    await channel.send(r[3])
+                    delreminder(r[0])
+            else:  # if we consume all within a minute, we need to pull more
+                repeat = bool(nr)  # but only if there were reminders
 
     @commands.group("remind", case_insensitive=True, invoke_without_command=True)
     async def remind(self, ctx: commands.Context, *msg):
         newdate = newreminder(ctx.message, " ".join(msg))
-        return await ctx.send("will remind on " + newdate.isoformat())
+        await ctx.send("will remind on " + newdate.isoformat())
+        return await self.reminding()
 
     @remind.command("del")
     async def remind_del(self, ctx, *msg):
@@ -25,7 +62,12 @@ class RemindCog(commands.Cog, name="Remind"):
 
     @remind.command("list")
     async def remind_list(self, ctx):
-        await listreminder(ctx.message, mentionreplacer(self.client))
+        toshow = ""
+        for r in listreminder(ctx.message):
+            toshow += f"{datetime.datetime.fromtimestamp(int(r[2]), reference.LocalTimezone())}: {r[3]}\n"
+
+        toshow = re.sub(r"<@!?(.*?)>", mentionreplacer(self.client), toshow)
+        await ctx.channel.send("Reminders:\n" + toshow)
 
 
 def setup(client: commands.Bot):
--- NossiInterface/reminder.py
+++ NossiInterface/reminder.py
@@ -1,93 +1,102 @@
+import pathlib
 import re
+import sqlite3
 import time
 from datetime import datetime, timedelta
-from typing import Union
+from typing import List, Tuple
 
 import discord
-from discord import abc
 from pytz import reference
 
-import Data
-
-remindfile = "reminders.txt"
-Data.append(remindfile, "")
 last = {}
 delete = []
 
+date_formats = [
+    "%d.%m.%y %H:%M:%S",
+    "%d.%m.%Y %H:%M:%S",
+    "%d.%m.%Y",
+    "%d.%m %H:%M:%S",
+    "%d.%m.%Y %H:%M",
+    "%d.%m %H:%M",
+    "%d.%m",
+    "%H:%M:%S",
+    "%H:%M",
+    "%y-%m-%d %H:%M:%S",
+    "%Y-%m-%d %H:%M:%S",
+    "%Y-%m-%d",
+    "%y-%m-%d %H:%M",
+    "%Y-%m-%d %H:%M",
+]
+days = r"(?P<days>\d+)\s*([d:]|days?)"
+hours = r"(?P<hours>\d+)\s*([h:]|hours?)"
+mins = r"(?P<minutes>\d+)\s*([m:]|min(utes?)?)"
+seconds = r"(?P<seconds>\d+)\s*(s(ec(onds?))?)"
 
-async def reminders(getchannel):
-    rembuf = []
-    nexttime = 10
-    now = time.time()
-    for line in Data.get(remindfile).splitlines():
-        if not line.strip():
-            continue
-        channelid, date, message = [p.strip() for p in line.split(";")]
-        channel: Union[discord.TextChannel, abc.PrivateChannel] = getchannel(
-            int(channelid)
+
+def setup_db():
+    path = pathlib.Path(__file__).parent / "remind.db"
+    preexisting = path.exists()
+    con = sqlite3.connect(path)
+    if not preexisting:
+        con.execute(
+            "CREATE TABLE IF NOT EXISTS reminders ("
+            "id integer PRIMARY KEY,"
+            "channel int NOT NULL,"
+            "executiondate DATE,"
+            "message TEXT);"
         )
-        if channel is None:
-            # connection problems
-            rembuf.append(line)
-            continue
-        if line in delete:
-            delete.remove(line)
-            await channel.send(message + " has been deleted")
-            continue
-        date = float(date)
-        if date < now:
-            await channel.send(message)
-        else:
-            rembuf.append(line)
-        nexttime = max(0, min(nexttime, date - now))
-    while delete:
-        d = delete.pop()
-        await getchannel(int(d[0])).send(
-            f"could not delete {d} from "
-            f"{list(tuple(p.strip() for p in line.split(';')) for line in Data.get(remindfile).splitlines())}"
-        )
-    Data.set(remindfile, "\n".join(rembuf) + "\n")
-    return nexttime
+    return con
 
 
-def time_eat(inp: str):
-    inp = inp.strip()
-    m = re.match(r"^[ A-Za-z]*", inp)
-    inp = inp[len(m.group(0)) :]
-    if m.group(0).strip() == "in":
-        if inp[:8].count(":") == 2:
-            inp = inp.replace(":", "h", 1).replace(":", "m", 1)
-        elif inp[:5].count(":") == 1:
-            inp = inp.replace(":", "m", 1)
-    rel = re.match(
-        r"^(?P<complete>(((?P<hours>\d+) ?[h] ?)?(?P<minutes>\d+) ?[m] ?)?(?P<seconds>\d+) ?[s]? )",
-        inp + " ",
+reminddb = setup_db()
+
+
+def next_reminders():
+    cur = reminddb.cursor()
+    return cur.execute(
+        "SELECT * FROM reminders ORDER BY executiondate DESC LIMIT 3"
+    ).fetchall()
+
+
+def save_reminder(date, channel, message):
+    cur = reminddb.cursor()
+    cur.execute(
+        "INSERT INTO reminders(executiondate,channel,message) VALUES (?,?,?)",
+        (date, channel, message),
     )
-    if rel:
-        msg = inp[len(rel.group("complete")) :]
-        h = int(rel.group("hours") or 0)
-        m = int(rel.group("minutes") or 0)
-        s = int(rel.group("seconds") or 0)
-        return h * 60 * 60 + m * 60 + s, msg
+    reminddb.commit()
+
+
+def extract_time_delta(inp: str):
+    inp = inp.strip()
+    if inp.startswith("in") or any(x in inp[:8] for x in "dhms"):
+        inp = inp[2:].strip() if inp.startswith("in") else inp
+
+        rel = (
+            re.match(r"\s*".join((days, hours, mins, seconds)) + r"?\s*", inp)
+            or re.match(r"\s*".join((hours, mins, seconds)) + r"?\s*", inp)
+            or re.match(r"\s*".join((hours, mins)) + r"?\s*", inp)
+            or re.match(seconds + r"\s*", inp)  # seconds without optional ending
+            or re.match(
+                r"(?=\d)((?P<days>\d+)\s*d\s*)?"
+                r"((?P<hours>\d+)\s*h\s*)?"
+                r"((?P<minutes>\d+)\s*m?\s*)?"
+                r"((?P<seconds>\d+)\s*s?)?\s*",
+                inp,
+            )
+        )
+        if rel:
+            msg = inp[rel.end() :]
+            d = int(rel.groupdict().get("days") or 0)
+            h = int(rel.groupdict().get("hours") or 0)
+            m = int(rel.groupdict().get("minutes") or 0)
+            s = int(rel.groupdict().get("seconds") or 0)
+            return d * 24 * 3600 + h * 3600 + m * 60 + s, msg
+    else:
+        inp = inp.removeprefix("at").removeprefix("on")
     date = re.match(r"^(?P<complete>[0-9.: -]*)", inp)
     msg = inp[len(date.group("complete")) :]
-
-    for fmt in [
-        "%d.%m.%y %H:%M:%S",
-        "%d.%m.%Y %H:%M:%S",
-        "%d.%m.%Y",
-        "%d.%m %H:%M:%S",
-        "%d.%m.%Y %H:%M",
-        "%d.%m %H:%M",
-        "%d.%m",
-        "%H:%M:%S",
-        "%H:%M",
-        "%y-%m-%d %H:%M:%S",
-        "%Y-%m-%d %H:%M:%S",
-        "%Y-%m-%d",
-        "%y-%m-%d %H:%M",
-        "%Y-%m-%d %H:%M",
-    ]:
+    for fmt in date_formats:
         try:
             d = datetime.strptime(date.group("complete").strip(), fmt)
             if d.year == 1900:
@@ -98,9 +107,12 @@ def time_eat(inp: str):
                 d.timestamp() - time.time(),
                 msg,
             )
-        except:
+        except Exception:
             continue
-    raise ValueError("unrecognizeable format:" + date.group("complete"))
+    try:
+        return int(date.group("complete")) * 60, msg  # minutes by default
+    except ValueError:
+        raise ValueError("unrecognizeable format:" + inp)
 
 
 def newreminder(message: discord.Message, msg: str):
@@ -110,28 +122,27 @@ def newreminder(message: discord.Message, msg: str):
         mention = message.author.mention + " "
     else:
         mention = ""
-    relatime, msg = time_eat(msg)
+    relatime, msg = extract_time_delta(msg)
+    msg = msg.removeprefix("that").strip()
+    if msg.lower().startswith("i "):
+        msg = "You" + msg[1:]
     date = time.time() + relatime
-    newline = (
-        ";".join((str(message.channel.id), str(round(date)), mention + msg)) + "\n"
-    )
-    Data.append(remindfile, newline)
+    save_reminder(date, message.channel.id, mention + msg)
     return datetime.fromtimestamp(int(date), reference.LocalTimezone())
 
 
-async def delreminder(message):
-    if last.get(str(message.channel.id), None) is None:
-        await message.add_reaction("😕")
-    else:
-        delete.append(last.get(str(message.channel.id)))
-        await message.add_reaction("👍")
+def delreminder(reminder_id):
+    cur = reminddb.cursor()
+    cur.execute(
+        "DELETE FROM reminders WHERE id=?",
+        (reminder_id,),
+    )
+    reminddb.commit()
 
 
-async def listreminder(message: discord.Message, mentionreplace):
-    toshow = ""
-    for line in Data.get(remindfile).splitlines():
-        if line.startswith(str(message.channel.id)):
-            channelid, date, msg = [p.strip() for p in line.split(";")]
-            toshow += f"{datetime.fromtimestamp(int(date), reference.LocalTimezone())}: {msg}\n"
-    toshow = re.sub(r"<@!?(.*?)>", mentionreplace, toshow)
-    await message.channel.send("Reminders:\n" + toshow)
+def listreminder(message: discord.Message) -> List[Tuple[int, int, float, str]]:
+    cur = reminddb.cursor()
+    return cur.execute(
+        "SELECT id, channel, executiondate, message FROM reminders WHERE channel=?",
+        (message.channel.id,),
+    ).fetchall()
