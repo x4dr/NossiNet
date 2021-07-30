b590c8e2a9127b47b2b08acce2817ff498434d4e
Maric <x4dr@hotmail.com>
2021-07-30 15:35:08 +0200

    remove travis support

--- NossiSite/webhook.py
+++ NossiSite/webhook.py
@@ -1,11 +1,3 @@
-import base64
-import json
-import logging
-from urllib.parse import parse_qs
-
-from OpenSSL.crypto import Error as SignatureError
-from OpenSSL.crypto import verify, load_publickey, FILETYPE_PEM, X509
-from flask import request, abort
 from github_webhook import Webhook
 
 from NossiSite.base import app as defaultapp
@@ -18,107 +10,7 @@ def register(app=None):
 
     @ghwh.hook()
     def on_push(req):
-        if app.config.get("TRAVIS"):
-            return
         if req["repository"]["name"] == "NossiNet":
             exit(4)
         else:
             print("got request from:", req["repository"]["name"])
-
-    @app.route("/travis", methods=["POST"])
-    def travis():
-        import requests
-
-        def _get_travis_public_keys():
-            """
-            Returns the PEM encoded public
-            key from the Travis CI /config endpoint
-            """
-            sig = []
-            for travis_config_url in [
-                "https://api.travis-ci.com/config",
-                "https://api.travis-ci.org/config",
-            ]:
-                response = requests.get(travis_config_url, timeout=10.0)
-                response.raise_for_status()
-                sig.append(
-                    response.json()["config"]["notifications"]["webhook"]["public_key"]
-                )
-            return sig
-
-        logger = logging.getLogger(__name__)
-        fh = logging.FileHandler("nossilog.log", mode="a")
-        fh.setLevel(logging.DEBUG)
-        logger.addHandler(fh)
-        logging.basicConfig(format="%(asctime)s %(levelname)s:%(message)s")
-        logger.setLevel(logging.DEBUG)
-
-        def check_authorized(sig, pkey, payload):
-            """
-            Convert the PEM encoded public key to a format palatable for pyOpenSSL,
-            then verify the signature
-            """
-            pkey_public_key = load_publickey(FILETYPE_PEM, pkey)
-            certificate = X509()
-            certificate.set_pubkey(pkey_public_key)
-            print("pubkey", pkey_public_key)
-            print("sig", sig.hex())
-            print("payload", payload)
-            verify(certificate, sig, payload, "sha1")
-
-        def _get_signature():
-            """
-            Extract the raw bytes of the request signature provided by travis
-            """
-            print("HEADERS:", request.headers)
-            return base64.b64decode(request.headers["Signature"])
-
-        signature = _get_signature()
-        body = request.get_data()
-        qs = parse_qs(body.decode())
-        json_payload = qs["payload"][0]
-        try:
-            public_keys = _get_travis_public_keys()
-        except requests.Timeout:
-            logger.error(
-                {
-                    "message": "Timed out when attempting to retrieve Travis CI public key"
-                }
-            )
-            return (
-                {"status": "failed"},
-                400,
-                {"Content-Type": "text/json; charset=utf-8"},
-            )
-        except requests.RequestException as e:
-            logger.error(
-                {
-                    "message": "Failed to retrieve Travis CI public key",
-                    "error": str(e.args),
-                }
-            )
-            return (
-                {"status": "failed"},
-                400,
-                {"Content-Type": "text/json; charset=utf-8"},
-            )
-        print("PUBLIC_KEYs:", "\n".join(public_keys))
-        try:
-            check_authorized(signature, public_keys[0], json_payload)
-        except SignatureError:
-            try:
-                check_authorized(signature, public_keys[1], json_payload)
-            except SignatureError:
-                logger.error(f"Unauthorized request by {request.headers['X-Real-Ip']}")
-                return abort(400)
-        json_data = json.loads(json_payload)
-        logger.info(f"RECEIVED: {json_data}")
-        if (
-            json_data["type"] == "push"
-            and json_data["state"] == "passed"
-            and json_data["branch"] == "master"
-        ):
-            print("restart triggered by travis")
-            exit(4)
-
-        return {"status": "received"}
--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 [![Known Vulnerabilities](https://snyk.io/test/github/x4dr/NossiNet/badge.svg)](https://snyk.io/test/github/x4dr/NossiNet)
-[![Build](https://travis-ci.com/x4dr/NossiNet.svg?branch=master)](https://github.com/x4dr/NossiNet/actions)
 [![Coverage Status](https://coveralls.io/repos/github/x4dr/NossiNet/badge.svg?branch=master)](https://coveralls.io/github/x4dr/NossiNet?branch=master)
 [![Validation](https://github.com/x4dr/NossiNet/workflows/Validation/badge.svg)](https://github.com/x4dr/NossiNet/actions)
