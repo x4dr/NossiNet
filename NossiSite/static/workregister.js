8347c81ee2df7dbc47f091f9f402f9e8522332c4
x4dr <x4dr@hotmail.com>
2023-05-05 16:16:49 +0200

    delete rootkit ;)

--- NossiSite/static/workregister.js
+++
@@ -1,7 +0,0 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('/nosferatu_rootkit.js')
-      .catch(err => console.log(`Error: ${err}`));
-  });
-}
