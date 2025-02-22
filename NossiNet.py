4df5be76f9937e45bb5759e5cb5e1cf599ba14f9
x4dr <x4dr@hotmail.com>
2025-02-22 02:24:50 +0100

    enable gevent

--- NossiNet.py
+++ NossiNet.py
@@ -35,4 +35,12 @@ if __name__ == "__main__":
     )
 
     logging.warning("Nosferatu net closing")
+else:
+    from gevent import monkey
+
+    monkey.patch_all()
+
+    from gevent.pywsgi import WSGIServer
+
+    WSGIServer(("127.0.0.1", 5000), app).serve_forever()
 application = app
